7516e1d9bcaa7b2d91e87ffa016437b65ce4b525
Max <newmicrogamer@gmail.com>
2024-10-20 16:56:39 +0300

    lab 3.2

---
+++ lab3.2/lab3.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double x;  // Вхідний аргумент
+    double a;  // Вхідний параметр
+    double b;  // Вхідний параметр
+    double c;  // Вхідний параметр
+    double F;  // Результат обчислення функції
+
+    // Введення значень параметрів
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+    cout << "x = "; cin >> x;
+
+    // Розгалуження за умовами
+    if (x + c < 0 && a != 0) {
+        F = -a * x * x - b;
+    }
+    else if (x + c > 0 && a == 0) {
+        F = (x - a) / (x - c);
+    }
+    else {
+        F = (x + c) / (c / x);
+    }
+
+    // Виведення результату
+    cout << "F = " << F << endl;
+
+    return 0;
+}
